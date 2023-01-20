2334f44f5912f01ebb698cd364c34b0498580207
Amaury Chabert <chabert.amaury2002@gmail.com>
2023-01-20 13:31:08 +0100

    first commmit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('Hello from the SW');
+window.addEventListener('load', () => {
+  registerSW();
+});
+
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator.serviceWorker.register('./sw.js');
+    } catch (e) {
+      console.log(`SW registration failed`);
+    }
+  }
+}
